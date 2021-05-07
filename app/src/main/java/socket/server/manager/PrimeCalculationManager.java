0bb126cf88ccadcef66f48c04ae07e99ae2ee7b9
Mahbubur Rahman Manik <mrmanik.kuet@gmail.com>
2021-05-07 19:37:28 +0600

    add one more comment

--- app/src/main/java/socket/server/manager/PrimeCalculationManager.java
+++ app/src/main/java/socket/server/manager/PrimeCalculationManager.java
@@ -1,6 +1,9 @@
 package socket.server.manager;
 
 public class PrimeCalculationManager {
+    // findPrimes finds the number of prime number inside given input n in most brute-force way (intentionally. don't optimize)
+    // this method will take different execution time based on given input
+
     public int findPrimes(int n) {
         int count = 0;
         for (int i = 2; i <= n; i++) {
